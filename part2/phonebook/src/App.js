86d10bda518477283f4c148ad458427e8bfd0ff6
ashna111 <ashnashah111@gmail.com>
2020-05-20 13:48:00 +0530

    2.11 done - get data for phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const hook = () => {
+        axios.get("http://localhost:3001/persons").then((response) => { setPersons(response.data); setPersonToShow(response.data) })
+    }
+    useEffect(hook, [])
+
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
-    const [personToShow, setPersonToShow] = useState(persons)
+    const [personToShow, setPersonToShow] = useState([])
 
     const getName = (event) => {
         setNewName(event.target.value)
