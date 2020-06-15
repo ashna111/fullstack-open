36c4a41d1056666f38e9c6d48c078b5b5f22f693
ashna111 <ashnashah111@gmail.com>
2020-06-15 11:22:01 +0530

    5.5 done

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,7 +3,22 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 import './index.css'
+
+const NewNote = ({ addBlog, title, author, url, setTitle, setAuthor, setUrl }) => {
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>title:<input value={title} onChange={({ target }) => setTitle(target.value)} /></div>
+        <div>author:<input value={author} onChange={({ target }) => setAuthor(target.value)} /></div>
+        <div>url:<input value={url} onChange={({ target }) => setUrl(target.value)} /></div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -110,13 +125,9 @@ const App = () => {
         <button onClick={logout}>logout</button>
         <Notification message={message} errorState={errorState} />
         <br />
-        <h2>create new</h2>
-        <form onSubmit={addBlog}>
-          <div>title:<input value={title} onChange={({ target }) => setTitle(target.value)} /></div>
-          <div>author:<input value={author} onChange={({ target }) => setAuthor(target.value)} /></div>
-          <div>url:<input value={url} onChange={({ target }) => setUrl(target.value)} /></div>
-          <button type="submit">create</button>
-        </form>
+        <Togglable buttonLabel="new note">
+          <NewNote addBlog={addBlog} title={title} author={author} url={url} setTitle={setTitle} setAuthor={setAuthor} setUrl={setUrl} />
+        </Togglable>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
